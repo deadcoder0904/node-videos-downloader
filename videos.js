95e5319827f6ceccff1980855a81755c3582a661
Akshay Kadam <akshaykadam0904@gmail.com>
2020-01-14 18:36:07 +0530

    Add Composable Gatsby Themes

--- videos.js
+++ videos.js
@@ -1,8 +1,44 @@
 const videos = [
   {
-    url: '',
-    name: '',
+    url: "https://vs2.coursehunter.net/egghead-cgthemes/lesson1.mp4",
+    name: "01. Set up a Shopify Account"
   },
-]
+  {
+    url: "https://vs2.coursehunter.net/egghead-cgthemes/lesson2.mp4",
+    name: "02. Set up a WordPress account"
+  },
+  {
+    url: "https://vs2.coursehunter.net/egghead-cgthemes/lesson3.mp4",
+    name: "03. Setup and Run a Gatsby Site"
+  },
+  {
+    url: "https://vs2.coursehunter.net/egghead-cgthemes/lesson4.mp4",
+    name: "04. Convert Gatsby Site to Yarn Workspace Project"
+  },
+  {
+    url: "https://vs2.coursehunter.net/egghead-cgthemes/lesson5.mp4",
+    name: "05. Overview of Gatsby Site Example Project"
+  },
+  {
+    url: "https://vs2.coursehunter.net/egghead-cgthemes/lesson6.mp4",
+    name: "06. Create a Marketing Gatsby Theme"
+  },
+  {
+    url: "https://vs2.coursehunter.net/egghead-cgthemes/lesson7.mp4",
+    name: "07. Build an Advanced Gatsby Theme Shopify"
+  },
+  {
+    url: "https://vs2.coursehunter.net/egghead-cgthemes/lesson8.mp4",
+    name: "08. Creating a Blog Theme"
+  },
+  {
+    url: "https://vs2.coursehunter.net/egghead-cgthemes/lesson9.mp4",
+    name: "09. Use Schema Customization to Support MDX and WordPress"
+  },
+  {
+    url: "https://vs2.coursehunter.net/egghead-cgthemes/lesson10.mp4",
+    name: "10. Powering BlogPost with WordPress and MDX"
+  }
+];
 
-module.exports = videos
+module.exports = videos;
